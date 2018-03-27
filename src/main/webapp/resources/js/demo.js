1a9a0cea4955f1e55ce33e1c58fd7678e36266c3
Sergey Sergienko <sergse2012@yandex.ru>
2018-03-27 23:08:33 +0300

    First commit

---
+++ src/main/webapp/resources/js/demo.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Сергей on 05.10.2016.
+ */
+function player() {
+    document.getElementById("player").style.visibility = 'visible';
+}
+function delet() {
+    document.getElementById("player").style.visibility = 'hidden';
+}
